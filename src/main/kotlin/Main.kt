3e2d96c17d0fa132f7e2bfc1252b7c356c23b63a
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-05-10 15:30:41 +0300

    assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,29 @@
 fun main(args: Array<String>) {
 
-    val new = threeNames("mercy","angeth","esther")
- println(new.contentToString())
+    var new = threeNames("mercy", "angeth", "esther")
+    println(new.contentToString())
     println(getAverage())
+    //Given a list of Person objects, each with the attributes, name, age, height,
+//and weight. Sort the list in order of descending age
+    data class ClassMates(val name: String, val age: Int, val height: Int, val weight: Int)
+
+    val mates = listOf(
+        ClassMates("Winfrida", 23, 180, 70),
+        ClassMates("Joyce", 20, 175, 55),
+        ClassMates("Annet", 22, 163, 45),
+    )
+    var sortedList = mates.sortedByDescending { it.age }
+    println(sortedList)
+    arr()
 }
+
 //3. Create a function that takes in 3 names and returns a string array containing all 3 names. (2 points)
 
 fun threeNames(person1: String,person2: String,person3:String):Array<String>{
   return arrayOf( person1,person2,person3)
 }
 
-//Given a list of Person objects, each with the attributes, name, age, height,
-//and weight. Sort the list in order of descending age
+
 
 
 //4. Write a function that takes in a list of Car objects each with a registration
@@ -32,6 +44,18 @@ fun getAverage(): Double{
 
     return newList
 
-
-
 }
+//Create one function that is given the below array:
+//var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+//(i) prints out the sum of the second and fifth elements (1 point)
+//(ii) prints out the index of 158 (1 point)
+//(iii) prints out the elements in ascending order.
+
+fun arr(){
+    var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+    var addition  = numbers[1]+numbers[4]
+    println(addition)
+    println(numbers.indexOf(158))
+    println(numbers.sorted())
+}
+
